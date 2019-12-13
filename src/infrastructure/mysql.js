052e108d82ba63ff2bbc6d0ee6080096a54fb153
Jens Segers <segers.jens@gmail.com>
2019-12-13 13:38:18 +0100

    :fire: Remove logging

--- src/infrastructure/mysql.js
+++ src/infrastructure/mysql.js
@@ -1,13 +1,6 @@
 import mysql from 'mysql2/promise';
 
 const createConnection = async function() {
-  console.log({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
-
   return await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
