94cc344013b6b83d5296a3d11968029f3f4ee425
Jens Segers <segers.jens@gmail.com>
2020-03-18 11:32:14 +0100

    :sparkles: Add search endpoint

--- src/domain/quotes/QuotesRepository.js
+++ src/domain/quotes/QuotesRepository.js
@@ -41,6 +41,23 @@ class QuotesRepository {
     return results[0];
   }
 
+  async search(text) {
+    const connection = await Mysql.getConnection();
+
+    const [
+      results,
+    ] = await connection.query(
+      'SELECT quotes.id, quotes.quote, COALESCE(authors.name, quotes.author) as author, COALESCE(authors.avatar, quotes.avatar) as avatar ' +
+        'FROM quotes ' +
+        'LEFT JOIN authors ON authors.id = quotes.author_id ' +
+        'WHERE quotes.quote LIKE ? ' +
+        'ORDER BY RAND() LIMIT 1',
+      [`%${text}%`],
+    );
+
+    return results ? results[0] : null;
+  }
+
   async add(quote) {
     const connection = await Mysql.getConnection();
 
