054bc15c9e8d9e94258d6aa9aa5e8eddbc6fc59e
Jens Segers <segers.jens@gmail.com>
2021-11-02 18:07:11 +0100

    :wrench: Connection timeout

--- src/infrastructure/Mysql.js
+++ src/infrastructure/Mysql.js
@@ -9,6 +9,7 @@ const getConnection = function () {
 			user: process.env.DB_USERNAME,
 			password: process.env.DB_PASSWORD,
 			database: process.env.DB_DATABASE,
+			connectTimeout: 5,
 		});
 	}
 
